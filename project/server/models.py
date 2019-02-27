ee16bfb4a13595ff0ed62609d1b291eed3ad2898
Jesse Khorasanee <37255664+Tehsurfer@users.noreply.github.com>
2019-02-27 15:53:37 +1300

    fix indentation (again)

--- project/server/models.py
+++ project/server/models.py
@@ -29,15 +29,15 @@ class User(db.Model):
         :return: string
         """
         try:
-        payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-            'iat': datetime.datetime.utcnow(),
-            'sub': user_id
-        }
-        return jwt.encode(
-            payload,
-            app.config.get('SECRET_KEY'),
-            algorithm='HS256'
-        )
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
         except Exception as e:
-        return e
+            return e
