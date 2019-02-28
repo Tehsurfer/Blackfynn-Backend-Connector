1a1fb8a029d857801e68926bffa04b8279462e8c
Jesse Khorasanee <37255664+Tehsurfer@users.noreply.github.com>
2019-02-28 14:15:44 +1300

    switch from running manager to app on file call

--- manage.py
+++ manage.py
@@ -75,4 +75,4 @@ def drop_db():
 if __name__ == '__main__':
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     context.load_cert_chain('/etc/letsencrypt/live/blackfynnpythonlink.ml/cert.pem', '/etc/letsencrypt/live/blackfynnpythonlink.ml/privkey.pem')
-    manager.run(host="127.0.0.1",port=133, ssl_context= context)
+    app.run(host="127.0.0.1",port=133, ssl_context= context)
