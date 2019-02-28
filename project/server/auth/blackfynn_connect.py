a755fe21cc207b4905092267a4f46a25afc5ffe7
Jesse Khorasanee <37255664+Tehsurfer@users.noreply.github.com>
2019-03-01 12:03:44 +1300

    Created a module for creating Blackfynn keys

---
+++ project/server/auth/blackfynn_connect.py
@@ -0,0 +1,38 @@
+import requests
+
+class BlackfynnConnect(object):
+
+    def __init__(self, email, password, api_token=None, api_secret=None):
+        self.email = email
+        self.password = password
+        if api_token is None:
+            self.status_code, self.session_token = self.get_session_token(email, password)
+            if self.status_code is 200:
+                self.status_code, self.api_token, self.api_secret = self.create_keys(self.session_token)
+
+
+
+    def get_session_token(self, email, password):
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        data = '{"email": "' + email + '" ,"password" : "' + password + '"}'
+        response = requests.post('https://api.blackfynn.io/account/login', headers=headers, data=data)
+        if response.status_code is 200:
+            return (response.status_code, response.json()['sessionToken'] )
+        else:
+            print(response.status_code, response.content)
+            return (response.status_code, response.content)
+
+    def create_keys(self, session_token):
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        params = ( ('api_key', '4bd923ad-924c-4dfc-b500-9ae9902802d8'), )
+        data = '{"name": "Auckland Biongineering Institute Apps Key"}'
+        response = requests.post('https://api.blackfynn.io/token/', headers=headers, params=params, data=data)
+        if response.status_code is 200:
+            return (response.status_code, response.json()['key'], response.json()['secret'])
+        else:
+            print(response.status_code, response.content)
+            return (response.status_code, response.content, None )
